d715dc7de855584933e79fbcb5f7789539cba989
vinicius henrique <vinicius13henrique01@gmail.com>
2026-01-16 14:56:50 -0300

    Primeiro commit

---
+++ src/test/java/com/example/crudusuarios/CrudusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
