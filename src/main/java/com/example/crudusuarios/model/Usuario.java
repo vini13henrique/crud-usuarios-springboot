b7418e4e503c0ad5786c81e754ec0d38b2c28141
vinicius henrique <vinicius13henrique01@gmail.com>
2026-01-16 15:21:52 -0300

    criado a classe usuario

---
+++ src/main/java/com/example/crudusuarios/model/Usuario.java
@@ -0,0 +1,55 @@
+package com.example.crudusuarios.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public Usuario(int id, String nome, String email, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public Usuario(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
