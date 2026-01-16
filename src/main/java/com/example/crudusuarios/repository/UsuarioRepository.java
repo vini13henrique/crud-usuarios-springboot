a0555690b348f49bf0ba0e9271f0f1e171101496
vinicius henrique <vinicius13henrique01@gmail.com>
2026-01-16 15:25:28 -0300

    criado a classe repository

---
+++ src/main/java/com/example/crudusuarios/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.example.crudusuarios.repository;
+
+import com.example.crudusuarios.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
+
+}
